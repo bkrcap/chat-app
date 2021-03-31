a34d47c22eec5ba943b75cf113e92d13ec4a1a2e
spiderweb0802 <81157779+bkrcap@users.noreply.github.com>
2021-03-31 14:37:27 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,22 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(essage_params)
+    if @message.save
+      redirecto_to
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id)
   end
 end
